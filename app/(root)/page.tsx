a3f55091719e142e8fedd86b36181de48eed1543
samblaha-mri <sam.blaha@mrisoftware.com>
2025-01-31 14:57:50 -0500

    sample products & product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/db/sample-data'
+
 const Homepage = () => {
-  return <>Prostore</>
+  return (
+    <div>
+      <h2>Latest Products</h2>
+      <ProductList
+        title="Newest Arrivals"
+        data={sampleData.products}
+        limit={4}
+      />
+    </div>
+  )
 }
 
 export default Homepage
