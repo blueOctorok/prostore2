b46185417e90ac03f6157c72eb935dc0ce80c165
blahablast <blahasam@gmail.com>
2025-02-02 13:04:21 -0500

    credentials sign in form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -12,6 +12,7 @@ import {
   CardTitle
 } from '@/components/ui/card'
 import { APP_NAME } from '@/lib/constants'
+import CredentialsSignInForm from './credentials-signin-form'
 
 export const metadata: Metadata = {
   title: 'Sign In'
@@ -36,7 +37,9 @@ const SignIn = () => {
             Select a method to sign in to your account
           </CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">{/* FORM HERE */}</CardContent>
+        <CardContent className="space-y-4">
+          <CredentialsSignInForm />
+        </CardContent>
       </Card>
     </div>
   )
