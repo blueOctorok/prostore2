d93bcea083eb0805ab04ada963bc97b7091e81b9
blahablast <blahasam@gmail.com>
2025-02-05 17:59:28 -0500

    customize token with jwt callback

--- auth.ts
+++ auth.ts
@@ -55,14 +55,42 @@ export const config = {
     })
   ],
   callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async session({ session, user, trigger, token }: any) {
       session.user.id = token.sub
+      session.user.role = token.role
+      session.user.name = token.name
 
       // If there is an update, set the user name
-      if (trigger === 'update') {
+      if (trigger === 'update' && token.name) {
         session.user.name = user.name
       }
       return session
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    async jwt({ token, user, trigger, session }: any) {
+      // Assign user fields to token
+      if (user) {
+        token.role = user.role
+
+        // If user has no name, use email as their default name
+        if (user.name === 'NO_NAME') {
+          token.name = user.email!.split('@')[0]
+
+          // Update the user in the database with the new name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name }
+          })
+        }
+      }
+
+      // Handle session updates (e.g., name change)
+      if (session?.user.name && trigger === 'update') {
+        token.name = session.user.name
+      }
+
+      return token
     }
   }
 } satisfies NextAuthConfig
